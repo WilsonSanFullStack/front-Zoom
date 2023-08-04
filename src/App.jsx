e886b8a661cbf31328c7c1960edb3da602bbd54a
ryuksan <clinicasystemlab@gmail.com>
2023-08-03 22:37:44 -0500

    arreglo de vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const handlerSubmit = (text) => {
+
+  };
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+            <div>
+                <h2>AdultWork</h2>
+                <textarea
+                  class="tex"
+                  name="adult"
+                  id="adults"
+                  cols="100"
+                  rows="15"
+                  placeholder="
+                Pege aqui el o los cortes de AdultWork
+                Funcionalidad completa cortes listos trabajando en parciales 60% completo"
+                ></textarea>
+                <div>
+                  <button>CORTE</button>
+                  <button>PARCIAL</button>
+                </div>
+            </div>
+            <div>
+                <table>
+                </table>
+              <div><button>ENVIAR</button></div>
+              <div><button></button></div>
+            </div>
+          </div>
     </>
   )
 }
