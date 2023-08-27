10b224a59a5dbeb42ff67e8df0408b479ff643c0
ryuksan <clinicasystemlab@gmail.com>
2023-08-27 03:18:04 -0500

    adfada

--- src/redux/actionRegistroUser.js
+++ src/redux/actionRegistroUser.js
@@ -7,12 +7,11 @@ const REGISTRO = import.meta.env.VITE_REACT_APP_URL_REGISTRO;
 
 
 export const registroUser = (input) => {
-  console.log(input);
   return async (dispatch) => {
     try {
       const endpoint = `${URL}/${REGISTRO}`;
       const { data } = await axios.post(endpoint, { input });
-      // console.log(data)
+      console.log(data)
       dispatch({
         type: RU,
         payload: data,
